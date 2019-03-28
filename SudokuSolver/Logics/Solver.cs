d2b0a8d30306fb6794207a9992724de920529760
jan bos <max.bos217@gmail.com>
2019-03-28 14:29:50 +0100

    HEEEEEEEEEEEEEEEEEEEEEEEELP 1

--- SudokuSolver/Logics/Solver.cs
+++ SudokuSolver/Logics/Solver.cs
@@ -13,34 +13,51 @@ namespace SudokuSolver.Logics
         {
             int[] IntArray = ToIntArray(sudoku);
             int[] IntArrayCount = new int[81];
-            for (int i = 0; i < IntArrayCount.Length; i++)
+            for (int i1 = 0; i1 < IntArrayCount.Length; i1++)
             {
-                if (IntArray[i] > 0) {
-                    IntArrayCount[i] = -1;
+                if (IntArray[i1] > 0) {
+                    IntArrayCount[i1] = -1;
                 } else {
-                    IntArrayCount[i] = 0;
+                    IntArrayCount[i1] = 0;
                 }
                 
             }
-            for (int i = 0; i < IntArray.Length; i++)
+            int i = 0;
+            int high = 0;
+            while (i < IntArray.Length)
             {
+                high = Math.Max(high, i);
                 if (IntArrayCount[i] > -1) {
                     List<int> found = new List<int>();
-                    found.AddRange(CheckX(i, IntArray));
                     found.AddRange(CheckY(i, IntArray));
+                    found.AddRange(CheckX(i, IntArray));
                     found.AddRange(CheckGrid(i, IntArray));
                     found = RemoveNumbers(found, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-                    if (found.Count == 0) {
+                    //Debug.WriteLine("i="+i.ToString()+";");
+                    //Debug.WriteLine("intcount = "+IntArrayCount[i].ToString());
+                    //Debug.WriteLine("foundcount = " + found.Count.ToString());
+                    if (found.Count == 0 || IntArrayCount[i] >= found.Count) {
                         IntArrayCount[i] = 0;
+                        IntArray[i] = 0;
                         i--;
+                        while (IntArrayCount[i] == -1)
+                        {
+                            i--;
+                        }
+                        IntArray[i] = 0;
                         IntArrayCount[i]++;
                     }
                     else
                     {
+                        //Debug.WriteLine("found = " + found[IntArrayCount[i]].ToString());
                         IntArray[i] = found[IntArrayCount[i]];
+                        i++;
                     }
                 }
-                Debug.WriteLine(i.ToString());
+                else
+                {
+                    i++;
+                }
             }
 
             return ToSudoku(IntArray);
@@ -50,9 +67,10 @@ namespace SudokuSolver.Logics
             int[][] sudoku = new int[9][];
             for (int i = 0; i < sudoku.Length; i++)
             {
+                sudoku[i] = new int[9];
                 for (int pos = 0; pos < 9; pos++)
                 {
-                    int x = i - i % 9 + pos;
+                    int x = i * 9 + pos;
                     sudoku[i][pos] = intArray[x];
                 }
             }
@@ -88,7 +106,8 @@ namespace SudokuSolver.Logics
             List<int> found = new List<int>();
             for (int i = 0; i < 9; i++)
             {
-                int x = pos - pos % 9 + i * 9;
+                int x = pos % 9 + i * 9;
+                //Debug.WriteLine(x.ToString() + " = POS");
                 found.Add(intArray[x]);
             }
             return found;
@@ -96,12 +115,28 @@ namespace SudokuSolver.Logics
         public List<int> CheckGrid(int pos, int[] intArray)
         {
             List<int> found = new List<int>();
+
+            List<int> hardList = new List<int> {
+                0,0,0,4,4,4,6,6,6,
+                0,0,0,4,4,4,6,6,6,
+                0,0,0,4,4,4,6,6,6,
+                27,27,27,30,30,30,33,33,33,
+                27,27,27,30,30,30,33,33,33,
+                27,27,27,30,30,30,33,33,33,
+                54,54,54,57,57,57,60,60,60,
+                54,54,54,57,57,57,60,60,60,
+                54,54,54,57,57,57,60,60,60
+            };
+
             for (int x = 0; x < 3; x++)
             {
                 for (int y = 0; y < 3; y++)
                 {
-                    int finalPos = pos - pos % 9 + x + y * 3;
-                    found.Add(intArray[finalPos]);
+                    //int col = pos % 9 + x;
+                    //int row = pos / 9 + y;
+                    Debug.WriteLine(hardList[pos] + x + y * 9);
+                    //found.Add(intArray[col + 9 * row]);
+                    found.Add(intArray[hardList[pos] + x + y * 9]);
                 }
             }
             return found;
